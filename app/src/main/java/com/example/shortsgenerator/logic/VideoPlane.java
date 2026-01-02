23933b2f8dd1bdba11f5c7e4f7cc7b7f92602779
aadmiin929-dev <aadmiin929@gmail.com>
2026-01-02 09:34:23 +0500

    Create VideoPlane.java

---
+++ app/src/main/java/com/example/shortsgenerator/logic/VideoPlane.java
@@ -0,0 +1,12 @@
+package com.example.shortsgenerator.logic;
+
+import java.util.List;
+
+public class VideoPlan {
+
+    public List<Scene> scenes;
+
+    public VideoPlan(List<Scene> scenes) {
+        this.scenes = scenes;
+    }
+}
