1a36813fca92a38450ce810a78cf6d13ee5e2914
aadmiin929-dev <aadmiin929@gmail.com>
2026-01-02 09:44:31 +0500

    Update scene.java

--- app/src/main/java/com/example/shortsgenerator/logic/scene.java
+++ app/src/main/java/com/example/shortsgenerator/logic/scene.java
@@ -4,9 +4,11 @@ public class Scene {
 
     public String text;
     public int durationSec;
+    public Mood mood;
 
-    public Scene(String text, int durationSec) {
+    public Scene(String text, int durationSec, Mood mood) {
         this.text = text;
         this.durationSec = durationSec;
+        this.mood = mood;
     }
 }
