119a5467064f675ff168e4bf450b9424347cde67
aadmiin929-dev <aadmiin929@gmail.com>
2026-01-02 09:32:56 +0500

    Create scene.java

---
+++ app/src/main/java/com/example/shortsgenerator/logic/scene.java
@@ -0,0 +1,12 @@
+package com.example.shortsgenerator.logic;
+
+public class Scene {
+
+    public String text;
+    public int durationSec;
+
+    public Scene(String text, int durationSec) {
+        this.text = text;
+        this.durationSec = durationSec;
+    }
+}
