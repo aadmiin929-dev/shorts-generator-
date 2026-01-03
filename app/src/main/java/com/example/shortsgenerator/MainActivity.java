a7232e6fdc3281963e6771bf1b4fffc08c8e1676
aadmiin929-dev <aadmiin929@gmail.com>
2026-01-03 10:30:27 +0500

    Update MainActivity.java

--- app/src/main/java/com/example/shortsgenerator/MainActivity.java
+++ app/src/main/java/com/example/shortsgenerator/MainActivity.java
@@ -1,23 +1,13 @@
 package com.example.shortsgenerator;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        TextView textView = new TextView(this);
-        textView.setTextSize(16f);
-        textView.setText(
-                "Это первое предложение.\n" +
-                "Это второе предложение.\n" +
-                "Это третье предложение."
-        );
-
-        setContentView(textView);
+        setContentView(R.layout.activity_main);
     }
 }
